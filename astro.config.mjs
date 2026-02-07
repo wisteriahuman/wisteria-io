5f5ab974e17efae4ac1fc951b145b0f78a7050ea
wisteria <wisteria@wisterianoMac-mini.local>
2026-02-07 23:57:19 +0900

    ポートフォリオ構築

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     server: {
       allowedHosts: ['wisteria-io.com', 'www.wisteria-io.com'],
     },
+
+    plugins: [tailwindcss()],
   },
 });
