1d0ed40ae7d8d9e9191a865f80dd4697a225ac2d
wisteria <wisteria@wisterianoMac-mini.local>
2026-02-08 00:29:37 +0900

    wisteria中心に変更、プロジェクト形式刷新、ファビコン変更、devツールバー無効化

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: { enabled: false },
   vite: {
     server: {
       allowedHosts: ['wisteria-io.com', 'www.wisteria-io.com'],
