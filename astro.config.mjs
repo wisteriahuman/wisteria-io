5dcaea701722614a1691288183d8053130d23ba9
wisteria <wisteria@wisterianoMac-mini.local>
2026-02-26 15:20:38 +0900

    add: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wisteria-io.com',
+  integrations: [sitemap()],
   devToolbar: { enabled: false },
   markdown: {
     shikiConfig: {
